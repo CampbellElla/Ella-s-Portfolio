2af03d5131c945adbf5555d8be18d5ee95d19bd6
Campbell Emmanuella <campbellemmanuella17@gmail.com>
2025-06-02 16:38:00 +0100

    ella's portfolio

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Experience from './pages/Experience'
+import Contact from './pages/contact'
+import Header from './ui/Header'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter
+      >
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/experience' element={<Experience />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/ui/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FaUserEdit } from 'react-icons/fa'
+import { MdOutlineAddToHomeScreen, MdWork } from 'react-icons/md'
+import { RiContactsFill } from 'react-icons/ri'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+      <nav className='flex items-center justify-center gap-[5%] p-5 bg-green-600 text-white shadow-lg'>
+              <Link to="/" className='flex items-center gap-1'><MdOutlineAddToHomeScreen /> Home</Link>
+              <Link to="/about" className='flex items-center gap-1'><FaUserEdit />About</Link>
+              <Link to="/experience" className='flex items-center gap-1'><MdWork />Experience</Link>
+              <Link to="/contact" className='flex items-center gap-1'><RiContactsFill />Contact</Link>
+    </nav>
+  )
+}
+
+export default Header
